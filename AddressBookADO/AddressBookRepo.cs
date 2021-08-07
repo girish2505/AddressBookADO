c4d4cb134624a0ea293e9362695495b4b6c517a0
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-08-07 18:34:17 +0530

    connecting to database

---
+++ AddressBookADO/AddressBookRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookADO
+{
+    public class AddressBookRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBookSystem;Integrated Security=True;";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
---
+++ AddressBookADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book ADO!");
+        }
+    }
+}
