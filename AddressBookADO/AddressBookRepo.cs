5418c14b708dc778a57ccd7e75195ba5437fb567
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-08-09 23:26:15 +0530

    insert data into the table

--- AddressBookADO/AddressBookRepo.cs
+++ AddressBookADO/AddressBookRepo.cs
@@ -11,18 +11,18 @@ namespace AddressBookADO
         SqlConnection sqlConnection = new SqlConnection(connectionString);
         public int RetrieveData()
         {
-            
+
             AddressBookModel model = new AddressBookModel();
-            
+
             int count = 0;
             using (sqlConnection)
             {
-                //Retrieve query
+                
                 string query = @"select * from Address_Book_Table";
                 SqlCommand command = new SqlCommand(query, this.sqlConnection);
-                //open sql connection
+                
                 sqlConnection.Open();
-                //sql reader to read data
+                
                 SqlDataReader sqlDataReader = command.ExecuteReader();
                 if (sqlDataReader.HasRows)
                 {
@@ -53,6 +53,36 @@ namespace AddressBookADO
             addressBook.relationType = Convert.ToString(sqlDataReader["RelationType"]);
             Console.WriteLine("FirstName :{0} LastName :{1} Address :{2} City :{3} State :{4} ZipCode :{5} PhoneNum :{6} EmailId :{7} AddressBookName :{8} RelationType :{9} ", addressBook.firstName, addressBook.lastName, addressBook.address, addressBook.city, addressBook.stateName, addressBook.zipCode, addressBook.phonenum, addressBook.emailId, addressBook.addrBookName, addressBook.relationType);
             Console.WriteLine("\n");
+        }
+        public int InsertIntoTable(AddressBookModel addressBook)
+        {
+            int count = 0;
+            using (sqlConnection)
+            {
+                SqlCommand sqlCommand = new SqlCommand("dbo.InsertTable", this.sqlConnection);
+
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@FirstName", addressBook.firstName);
+                sqlCommand.Parameters.AddWithValue("@LastName", addressBook.lastName);
+                sqlCommand.Parameters.AddWithValue("@Address", addressBook.address);
+                sqlCommand.Parameters.AddWithValue("@City", addressBook.city);
+                sqlCommand.Parameters.AddWithValue("@StateName", addressBook.stateName);
+                sqlCommand.Parameters.AddWithValue("@ZipCode", addressBook.zipCode);
+                sqlCommand.Parameters.AddWithValue("@Phonenum", addressBook.phonenum);
+                sqlCommand.Parameters.AddWithValue("@EmailId", addressBook.emailId);
+                sqlCommand.Parameters.AddWithValue("@AddressBookName", addressBook.addrBookName);
+                sqlCommand.Parameters.AddWithValue("@RelationType", addressBook.relationType);
+                
+                sqlConnection.Open();
+                
+                int result = sqlCommand.ExecuteNonQuery();
+                if (result != 0)
+                {
+                    count++;
+                    Console.WriteLine("Inserted Successfully");
+                }
+            }
+            return count;
         }
     }
 }
--- AddressBookADO/Program.cs
+++ AddressBookADO/Program.cs
@@ -8,12 +8,26 @@ namespace AddressBookADO
         {
             Console.WriteLine("Address Book ADO!");
             AddressBookRepo addrBookRepo = new AddressBookRepo();
+            AddressBookModel addrBook = new AddressBookModel();
             Console.WriteLine("1.Connecting to DB And Retrieve the data from sql server");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     addrBookRepo.RetrieveData();
+                    break;
+                case 2:
+                    addrBook.firstName = "Anish";
+                    addrBook.lastName = "guptha";
+                    addrBook.address = "velacherry";
+                    addrBook.city = "Chennai";
+                    addrBook.stateName = "Tamilnadu";
+                    addrBook.zipCode = "600015";
+                    addrBook.phonenum = 932415670;
+                    addrBook.emailId = "a.g@gmail.com";
+                    addrBook.addrBookName = "Cousin";
+                    addrBook.relationType = "Family";
+                    addrBookRepo.InsertIntoTable(addrBook);
                     break;
             }
         }
